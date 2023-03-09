852a70e8dc74a4cd200eb5aad601dd0953946d17
bintechvn <127424945+bintechvn@users.noreply.github.com>
2023-03-10 00:19:28 +0700

    Update Vultr-No-Auth.sh

--- Vultr-No-Auth.sh
+++ Vultr-No-Auth.sh
@@ -82,7 +82,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "mcproxy/mcproxy032023/$interface/$IP4/$port/$(gen64 $IP6)"
+        echo "bintechproxy/bintechpass/$interface/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
