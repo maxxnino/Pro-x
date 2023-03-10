05ea24e841ef8e83a20b679f19a44cc32874d617
bintechvn <127424945+bintechvn@users.noreply.github.com>
2023-03-10 12:16:24 +0700

    Update Lino.sh

--- Lino.sh
+++ Lino.sh
@@ -127,13 +127,13 @@ interface=eth0
 #    esac
 #done
 Auth=strong
-User=mcproxy
-Pass=mcproxy032023
+User=MKproxy
+Pass=MKpasswd
 
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=35249
+LAST_PORT=33000
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
