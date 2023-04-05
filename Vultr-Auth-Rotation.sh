19af21956a63a8071c71e5474079cb82092915b2
bintechvn <127424945+bintechvn@users.noreply.github.com>
2023-04-05 09:20:36 +0700

    Update Vultr-Auth-Rotation.sh

--- Vultr-Auth-Rotation.sh
+++ Vultr-Auth-Rotation.sh
@@ -136,7 +136,7 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-wget "https://raw.githubusercontent.com/bintechvn/Pro-x/main/Rotation.sh" --output-document=/root/Rotation.sh
+wget "https://raw.githubusercontent.com/bintechvn/Pro-x/main/Vultr-Rotation.sh" --output-document=/root/Rotation.sh
 chmod 777 /root/Rotation.sh
 cat >>/var/spool/cron/root<<EOF
 #day - time
